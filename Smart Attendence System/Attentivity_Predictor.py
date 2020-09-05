5e94d3ac5e3b918b464f2069f810761fd35234b5
Gourab Chakraborty <19bec014@iiitdwd.ac.in>
2020-09-05 20:03:57 +0530

    Bug Fixes
    Unit Testing done. To see the test cases check out test.txt

--- Smart Attendence System/Attentivity_Predictor.py
+++ Smart Attendence System/Attentivity_Predictor.py
@@ -12,10 +12,10 @@ def eye_aspect_ratio(eye):
 	ear = (A + B) / (2.0 * C)
 	return ear
 	
-thresh = 0.30      #eye threshold set to 30% of fully awaken eye gave an optimum result of drowsiness 
+thresh = 0.25      #eye threshold set to 25% of fully awaken eye gave an optimum result of Inattentivness 
 frame_check = 20    #frame refresh rate set to 20 ms
 detect = dlib.get_frontal_face_detector()
-predict = dlib.shape_predictor("C:/Users/GOURAB/OneDrive/Desktop/Drowsiness_Detection-master/shape_predictor_68_face_landmarks.dat")  #change this to ur local location
+predict = dlib.shape_predictor("C:/Users/GOURAB/OneDrive/Desktop/Eduthon/Smart Attendence System/shape_predictor_68_face_landmarks.dat")  #change this to ur local location
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
@@ -23,6 +23,9 @@ cap=cv2.VideoCapture(0)
 flag=0
 inattentive_time=0 
 lecture_time=time.time() #starts count of lecture time
+count_inattentive = 0
+count_attentive = 0
+print("Lecture Running")
 while True:
 	ret, frame=cap.read()
 	frame = imutils.resize(frame, width=450)
@@ -41,34 +44,42 @@ while True:
 		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
 		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 		end=0
+		
 		if ear < thresh:
 			flag += 1
 			print (flag)
+			
 			start=time.time()
 			if flag >= frame_check:
 				# calculate time blocks for inattentive time and sum
 				
-				cv2.putText(frame, "****************ALERT!****************", (10, 30),
+				cv2.putText(frame, "**************ALERT!**************", (10, 30),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-				cv2.putText(frame, "****************ALERT!****************", (10,325),
+				cv2.putText(frame, "**************ALERT!**************", (10,325),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-			end=time.time()
-			end = end - start # end here counts inattentive time for each instance
-			inattentive_time = inattentive_time + end  
-				#print ("Drowsy")
+			count_inattentive = count_inattentive + 1  
+
+				
 				
 		else:
 			flag = 0
+			count_attentive = count_attentive + 1
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 	if key == ord("q"):
 		break
 current_time = time.time()
+#print("Sum is", count_attentive + count_inattentive)  
 lecture_time = current_time - lecture_time   #gives net lecture time, substract inattentive time to get attentive... calculate attentive percentage.
-attentive_time= lecture_time - inattentive_time
+#print("div is:", lecture_time / (count_attentive+ count_inattentive) )
+new_div = lecture_time / (count_attentive+ count_inattentive) 
+attentive_time = count_attentive * new_div
+inattentive_time = count_inattentive * new_div
 attendence_percentage= ( attentive_time / lecture_time ) * 100
-print("Inattentivity is:", inattentive_time ,"\n")
+
+print("**************************Lecture Over*********************\n")
 print("Lecture Time is:", lecture_time ,"\n")
+print("Inattentive Time is:", inattentive_time ,"\n")
 print("Attentive Time is:", attentive_time ,"\n")
 print("Today's attendence is:", attendence_percentage ," % \n")
 cv2.destroyAllWindows()

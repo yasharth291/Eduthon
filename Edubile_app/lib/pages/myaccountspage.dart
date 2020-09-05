842952aa8db9dd2e29c233c3b93fad480749be0a
Yasharth dubey <yashdubeywinner@gmail.com>
2020-09-06 03:28:35 +0530

    Add files via upload

--- Edubile_app/lib/pages/myaccountspage.dart
+++ Edubile_app/lib/pages/myaccountspage.dart
@@ -1,15 +1,72 @@
 import 'package:flutter/material.dart';
 
 import '../bloc.navigation_bloc/navigation_bloc.dart';
+import 'package:eduthon/pages/Info_card.dart';
+
+const email = 'XXXX@example.com';
+const phone = '+91 XXX XXXX XXX';
+const url = "Indian Institute of Information technology Dharwad";
+
 
 class MyAccountsPage extends StatelessWidget with NavigationStates {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "My Accounts",
-        style: TextStyle(fontWeight: FontWeight.w900, fontSize: 28),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              child: Image.asset("assets/student.png"),
+            ),
+            Text(
+              'ABC',
+              style: TextStyle(
+                fontSize: 40.0,
+                color:  Color.fromRGBO(126, 46, 132, 1),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '19XXX21',
+              style: TextStyle(
+                fontSize: 30.0,
+                color:  Color.fromRGBO(126, 46, 132, 1),
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: 200,
+              child: Divider(
+                color:Color.fromRGBO(230, 230, 250, 1),
+              ),
+            ),
+            InfoCard(
+              text: phone,
+              icon: Icons.phone,
+              onPressed: () {},
+            ),
+            InfoCard(
+              text: email,
+              icon: Icons.email,
+              onPressed: () {},
+            ),
+            InfoCard(
+              text: url,
+              icon: Icons.web,
+              onPressed: () {},
+            ),
+            InfoCard(
+              text: 'Mathura, India',
+              icon: Icons.location_city,
+            ),
+          ],
+        ),
       ),
+      backgroundColor: Color.fromRGBO(230, 230, 250, 1),
     );
   }
 }

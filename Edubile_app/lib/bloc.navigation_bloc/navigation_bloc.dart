43853337a752a04d6740365634f514a7574ebf66
Yasharth dubey <yashdubeywinner@gmail.com>
2020-09-06 04:17:59 +0530

    Add files via upload

--- Edubile_app/lib/bloc.navigation_bloc/navigation_bloc.dart
+++ Edubile_app/lib/bloc.navigation_bloc/navigation_bloc.dart
@@ -1,20 +1,19 @@
 import 'package:bloc/bloc.dart';
 import '../pages/myaccountspage.dart';
-import '../pages/myorderspage.dart';
-
+import '../pages/mycoursespage.dart';
 import '../pages/homepage.dart';
 
 enum NavigationEvents {
   HomePageClickedEvent,
   MyAccountClickedEvent,
-  MyOrdersClickedEvent,
+  MyCoursesClickedEvent,
 }
 
 abstract class NavigationStates {}
 
 class NavigationBloc extends Bloc<NavigationEvents, NavigationStates> {
   @override
-  NavigationStates get initialState => MyAccountsPage();
+  NavigationStates get initialState => HomePage();
 
   @override
   Stream<NavigationStates> mapEventToState(NavigationEvents event) async* {
@@ -25,8 +24,8 @@ class NavigationBloc extends Bloc<NavigationEvents, NavigationStates> {
       case NavigationEvents.MyAccountClickedEvent:
         yield MyAccountsPage();
         break;
-      case NavigationEvents.MyOrdersClickedEvent:
-        yield MyOrdersPage();
+      case NavigationEvents.MyCoursesClickedEvent:
+        yield courses();
         break;
     }
   }
